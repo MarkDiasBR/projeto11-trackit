f9b5f322ff135b64400cc82b81d8d3d6e1fd718b
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-03-20 07:26:10 -0300

    Layout tela de Cadastro

--- src/pages/CadastroPage/CadastroPage.js
+++ src/pages/CadastroPage/CadastroPage.js
@@ -1,17 +1,19 @@
 import { LoginElementsContainer, Logo, LoginForm, InputField, ButtonForm } from "./styled"
 import { Link } from "react-router-dom"
 
-export default function LoginPage() {
+export default function CadastroPage() {
     return(
         <LoginElementsContainer>
             <Logo src="./assets/img/Logo.png" alt="TrackIt Logo"/>
             <LoginForm>
                 <InputField type="email" placeholder="e-mail" />
                 <InputField type="password" placeholder="senha"/>
+                <InputField placeholder="nome"/>
+                <InputField placeholder="foto"/>
                 <ButtonForm type="submit">Entrar</ButtonForm>                               
             </LoginForm>
-            <Link to="/cadastro">
-                <p>Não tem uma conta? Cadastre-se!</p>
+            <Link to="/">
+                <p>Já tem uma conta? Faça login!</p>
             </Link>
         </LoginElementsContainer>
     )
